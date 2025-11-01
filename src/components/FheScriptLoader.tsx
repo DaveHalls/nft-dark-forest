482581e4e5d244022c94291533d534c539f4babc
Dave Hall <104647792+DaveHalls@users.noreply.github.com>
2025-11-01 12:27:34 +0100

    Update FheScriptLoader.tsx

--- src/components/FheScriptLoader.tsx
+++ src/components/FheScriptLoader.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script';
 export default function FheScriptLoader() {
   return (
     <Script
-      src="https://cdn.zama.ai/relayer-sdk-js/0.2.0/relayer-sdk-js.umd.cjs"
+      src="https://cdn.zama.org/relayer-sdk-js/0.2.0/relayer-sdk-js.umd.cjs"
       strategy="afterInteractive"
       crossOrigin="anonymous"
     />
